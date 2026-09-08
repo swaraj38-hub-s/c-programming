ba1520bc5e62be7098d20ee17c2a86f5fe42ab82
swaraj38-hub-s <swarajjadhav941@gmail.com>
2026-09-08 06:12:08 -0700

    Add files via upload

---
+++ AREA.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   clrscr();
+   int l,b;
+   printf("enter length and breadth:");
+   scanf("%d%d",&l,&b);
+   printf("area of rectangle=%d",l*b);
+   getch();
+}
