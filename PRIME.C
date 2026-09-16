8e16c8b1b1a2859f96677e04a210971c41f5b22a
Swaraj Jadhav <swarajjadhav941@gmail.com>
2026-09-16 05:57:02 -0700

    Add files via upload

---
+++ PRIME.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int i,n,c=0;
+  clrscr();
+
+  printf("Enter Any Number\n");
+  scanf("%d",&n);
+
+  for(i=1;i<=n;i++)
+  {
+   if(n%i==0)
+   c++;
+  f
+  }
+  if(c==2)
+  {
+   printf("prime");
+  }
+  else
+  {
+  printf("Not Prime");
+  }
+  getch();
+}
